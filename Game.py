7291bffd1b243569b8defc962a9d60293af892c5
Aditya Tomar <143313634+adityatomar25@users.noreply.github.com>
2023-09-29 22:55:16 +0530

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,1 +1,58 @@
+import random
+import tkinter as tk
+from tkinter import messagebox
 
+# Tarot card deck with meanings
+tarot_deck = {
+    "The Fool": "Beginnings, spontaneity, leap of faith",
+    "The Magician": "Manifestation, resourcefulness, power",
+    "The High Priestess": "Intuition, unconscious knowledge, mystery",
+    "The Empress": "Fertility, nurturing, abundance",
+    "The Emperor": "Authority, leadership, control",
+    
+}
+
+# Create an empty list to store card history
+card_history = []
+
+def draw_tarot_card():
+    return random.choice(list(tarot_deck.keys()))
+
+def interpret_card(card):
+    return tarot_deck.get(card, "Meaning not found")
+
+def draw_card_and_show_meaning():
+    card = draw_tarot_card()
+    meaning = interpret_card(card)
+    card_history.append(f"You drew: {card}\nMeaning: {meaning}")
+
+    # Show the drawn card and its meaning in a messagebox
+    messagebox.showinfo("Tarot Card Reading", f"You drew: {card}\nMeaning: {meaning}")
+
+def show_card_history():
+    # Display the card history in a separate window
+    history_text = "\n\n".join(card_history)
+    messagebox.showinfo("Card History", history_text)
+
+# Create the main GUI window
+root = tk.Tk()
+root.title("Tarot Card Reader")
+
+# Create a label
+label = tk.Label(root, text="Click 'Draw Card' to draw a Tarot card.")
+label.pack(pady=10)
+
+# Create a button to draw a card
+draw_button = tk.Button(root, text="Draw Card", command=draw_card_and_show_meaning)
+draw_button.pack(pady=10)
+
+# Create a button to show card history
+history_button = tk.Button(root, text="Card History", command=show_card_history)
+history_button.pack(pady=10)
+
+# Create a quit button
+quit_button = tk.Button(root, text="Quit", command=root.quit)
+quit_button.pack(pady=10)
+
+# Start the GUI event loop
+root.mainloop()
